c55ade8e98b653d4ea3f5412498741018a320fc2
Marco Julian Morales <marcojulian.morales@gmail.com>
2023-07-23 21:58:39 -0700

    Modulo 8 terminado Gifsapp

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './component/search-box/search-box.component';
 import { CardListComponent } from './component/card-list/card-list.component';
+import { GifsCardComponent } from './component/gifs-card/gifs-card.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -10,10 +12,12 @@ import { CardListComponent } from './component/card-list/card-list.component';
   declarations: [
     HomePageComponent,
     SearchBoxComponent,
-    CardListComponent
+    CardListComponent,
+    GifsCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent
